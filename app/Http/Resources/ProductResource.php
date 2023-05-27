57d3877a8d6cc3cefc29562162f93e78184b4cdc
aushev06 <34713423+aushev06@users.noreply.github.com>
2023-05-27 12:12:05 +0300

    Update ProductResource.php

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -15,13 +15,13 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-					'id'=>$this->id,
-					'name'=>$this->name,
-					'image_path'=>"app/storage/app/public/$this->image_path",
-					'description'=>$this->id,
-                    'price'=>$this->price,
-                    'fermer'=>$this->fermer,
-                    'category_id'=>$this->category_id
+            'id' => $this->id,
+            'name' => $this->name,
+            'image_path' => str_contains($this->image_path, 'https') ? $this->image_path : "/storage/$this->image_path",
+            'description' => $this->id,
+            'price' => $this->price,
+            'fermer' => $this->fermer,
+            'category_id' => $this->category_id
 
         ];
     }
